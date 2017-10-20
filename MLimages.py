82f6f2655eaaeffed2d37c560b52091237df6119
ajinkya933 <abobade@stevens.edu>
2017-10-20 16:44:54 -0500

    updated code to detect and display broken jpg files

--- MLimages.py
+++ MLimages.py
@@ -1,16 +1,24 @@
+# 10/20/2017
+# Created by Ajinkya Bobade
+#program to detect corrupt jpg images
+
+
 import os
 from os import listdir
 from PIL import Image
 count=0
-for filename in os.listdir('/Users/ajinkyabobade/Downloads/2/'):
+
+
+for filename in os.listdir('/Users/ajinkyabobade/Downloads/2/'):# mention your file path here for example in my case i have all the images in folder named "2" which is inside in downloads
     if filename.upper().endswith('.JPG'):
-     try:
-         img = Image.open('/Users/ajinkyabobade/Downloads/2/' + filename)
+     try:   # These next functions may produce an exception
+         img = Image.open('/Users/ajinkyabobade/Downloads/2/' + filename)# mention your file path here for example in my case i have all the images in folder named "2" which is inside in downloads
          img.verify()
-     except(IOError,SyntaxError)as e:
-         print('Bad file  :  '+filename)
+
+
+     except(IOError,SyntaxError)as e:# These are the exceptions we're looking for
+         print('Bad file  :  '+filename) # print out the names of corrupt files
          count=count+1
          print(count)
 
-#file_count=len(filename)
-#print(file_count)
+print('Im done')
